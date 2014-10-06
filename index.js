aff07a9b2f15b654cd0b539b5e67335b5b9f7212
Matias Singers <mail@matiassingers.com>
2014-10-06 21:50:55 +0800

    Create initial index.js logic for fetching Iconmart website

--- index.js
+++ index.js
@@ -1,5 +1,58 @@
 'use strict';
 
-module.exports = function(){
+var request = require('promise-request');
+var cheerio = require('cheerio');
+var _ = require('lodash');
+var fs = require('fs');
+var path = require('path');
 
+var $;
+
+function run(destination){
+  return fetchWebpage()
+    .then(handleItems)
+    .then(function writeFile(icons) {
+      var destinationPath = destination ? path.join(destination, 'data') : __dirname;
+
+      var file = path.join(destinationPath, 'icons-iconmart.json');
+      var data = JSON.stringify(icons, null, 2);
+
+      console.log('writing file to', file);
+
+      fs.writeFile(file, data, function(error) {
+        if(error)
+          return console.log(error);
+      });
+    });
+}
+
+function fetchWebpage(){
+  var url = 'http://iconmart.in/';
+
+  return request(url)
+    .then(function(body){
+      $ = cheerio.load(body);
+
+      return $('.preview ul .item-box a i').toArray();
+    });
+}
+
+function handleItems(items){
+  var icons = _.map(items, function(item){
+    var element = $(item);
+
+    return {
+      name: element.attr('class'),
+      tags: ''
+    };
+  });
+
+  return {
+    foundation: icons
+  };
+}
+
+module.exports = {
+  run: run,
+  fetchWebpage: fetchWebpage
 };
