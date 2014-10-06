383e0c1e78e05c1bfe1fad16e65233d5a65d7f2c
Matias Singers <mail@matiassingers.com>
2014-10-06 21:51:26 +0800

    Add simple tests

--- test.js
+++ test.js
@@ -1,10 +1,27 @@
 'use strict';
 
 var assert = require('assert');
+var cheerio = require('cheerio');
 var iconmartGlyphiconsCrawler = require('./');
 
-describe('initial', function(){
-  it('fail', function() {
-    assert(false);
+describe('fetchWebpage()', function(){
+  this.timeout(0);
+
+  var icons;
+  before(function(done){
+    iconmartGlyphiconsCrawler.fetchWebpage()
+      .then(function(response){
+        icons = response;
+
+        done();
+      });
+  });
+
+  it('should return array of icons', function(){
+    assert(icons.length);
+  });
+
+  it('each icon should have data-dropdown attr', function(){
+    assert(icons[0].attribs.class);
   });
 });
